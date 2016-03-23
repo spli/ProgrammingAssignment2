1e6f0a9f0bfe4cc14c4191be993f386a4cb2f63e
Simon Frost <spli@little-spli.com>
2016-03-23 19:46:01 +0000

    adds two functions to calculate and cache matrix inverses

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions compute the inverse of a matrix and 
+## caches the result
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a square invertible matrix argument and 
+## returns a list with functions to set a new matrix, 
+## get the current matrix, get the cached inverse and
+## set the inverse to be cached
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set, get = get, 
+             setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve takes a special matrix list returned by makeCacheMatrix and
+## computes and caches the inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if (!is.null(i)) {
+                message("returning cached result")
+                return(i)
+        } else {
+                data <- x$get()
+                i <- solve(data, ...)
+                x$setinverse(i)
+                return(i)
+        }
 }
